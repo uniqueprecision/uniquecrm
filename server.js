64422e734811c16ac277910d924ce7635b21dbbe
uniqueprecision <uniqueprecisioncrm@gmail.com>
2026-03-09 17:41:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ const path = require("path");
 
 
 const app = express();
-const PORT = 3000;
 
 
 /* ===============================
@@ -1436,6 +1435,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
+
 
 
 
